186d018e52ff23fe43d5d1a09aebee872baca2c4
Chirag <chirag1997aggarwal@gmail.com>
2021-11-25 06:28:49 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='House prices prediction with mlops',
+    author='Chirag1997aggarwal',
+    license='MIT',
+)
